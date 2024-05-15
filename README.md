9c5764f4ffebc195c7867fcb89f32322fa21c2a1
gustav <gustaavv.git@yahoo.com>
2024-05-15 20:00:54 +0800

    init, 0.1.4 version

---
+++ README.md
@@ -0,0 +1,52 @@
+# script-house
+
+该仓库是一个方便我日常使用的 python 模块 —— [`script-house`](https://pypi.org/project/script-house/) 。预计我的大部分 python 项目都会依赖这个基础模块。
+
+`script-house` 主要包括两个包：
+- `utils`：用于开发的工具类。
+- `ops`：用于日常使用的工具类（脚本）。
+
+# 使用 
+安装：
+```shell
+pip install script-house
+```
+
+保持最新版本：
+```shell
+pip install --upgrade script-house
+```
+
+
+
+
+
+
+# 开发者用
+> （提醒我自己如何用）
+ 
+ 
+## 如何构建
+
+```shell
+python.exe .\setup.py bdist_wheel sdist
+pip install .
+```
+
+
+## 如何上传到 pypi
+1. 拿 `api_token`
+
+2. 创建 .pypirc 文件，放到 `$HOME` 目录（Windows 的话，放到 `C:\Users\用户名` 下）
+   .pypirc 格式：
+
+    ```ini
+    [pypi]
+      username = __token__
+      password = <api_token>
+    ```
+
+3. `pip install wine`
+
+4. 上传：`twine upload dist/script_house-x.y.z-py3-none-any.whl`
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+    requirements = [r.strip() for r in requirements]
+
+setup(
+    name="script_house",
+    version="0.1.3",
+    description="a python script house containing handy functions for daily use",
+    packages=find_packages(),
+    url="https://github.com/gustaavv/script-house",
+    author="Gustav",
+    author_email="gustaavv.git@yahoo.com",
+    # python_requires=">=3.11",
+    install_requires=requirements,
+    classifiers=[
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Other Audience",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3"
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
